09088a15cb41ab0aa19556e6814c4af704310e37
Stanley-PC\Stanley <stanleychu56@gmail.com>
2017-02-19 02:59:45 -0800

    Added help wanted page

---
+++ application/controllers/Helpme.php
@@ -0,0 +1,12 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Helpme extends Application {
+	public function index() {
+		$this->data['pagebody'] = 'Help Wanted!';
+		$stuff = file_get_contents('../data/jobs.md');
+		$this->data['content'] = $this->parsedown->parse($stuff);
+		$this->render();
+	}
+}
